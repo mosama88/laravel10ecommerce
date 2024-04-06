fa23b9d2914d522c88ec70c89b42d515169bf141
mosama88 <mosama88@hotmail.com>
2024-04-06 02:02:35 +0200

    auth.admin

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -65,18 +65,42 @@
                                 </div>
                             </li>
                             <li class="onhover-dropdown">
-                                <div class="cart-media name-usr">
+                                <div class="cart-media name-usr mx-1">
+                                    @auth
+                                        <span>{{ Auth::user()->name }}</span>
+                                    @endauth
                                     <i data-feather="user"></i>
                                 </div>
                                 <div class="onhover-div profile-dropdown">
                                     <ul>
-                                        <li>
-                                            <a href="login.html" class="d-block">Login</a>
-                                        </li>
-                                        <li>
-                                            <a href="register.html" class="d-block">Register</a>
-                                        </li>
-
+                                        @if (Route::has('login'))
+                                            @auth
+                                                @if (Auth::user()->type === 'ADM')
+                                                    <li>
+                                                        <a href="{{ route('admin.index') }}" class="d-block">Dashboard</a>
+                                                    </li>
+                                                @else
+                                                    <li>
+                                                        <a href="{{ route('user.index') }}" class="d-block">My Account</a>
+                                                    </li>
+                                                @endif
+                                                <li>
+                                                    <a href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault();document.getElementById('frmlogout').submit(); "
+                                                        class="d-block text-red">Log Out</a>
+                                                </li>
+                                                <form id="frmlogout" action="{{ route('logout') }}" method="POST">
+                                                    @csrf
+                                                </form>
+                                            @else
+                                                <li>
+                                                    <a href="{{ route('login') }}" class="d-block">Login</a>
+                                                </li>
+                                                <li>
+                                                    <a href="{{ route('register') }}" class="d-block">Register</a>
+                                                </li>
+                                            @endauth
+                                        @endif
                                     </ul>
                                 </div>
                             </li>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +17,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [AppController::class, 'index'])->name('app.index') ;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/my-account', [UserController::class, 'index'])->name('user.index') ;
+});
+
+Route::middleware(['auth', 'auth.admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index') ;
+
+});
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
